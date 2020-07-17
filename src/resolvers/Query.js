b955e53005d106767d96df3280d7f88b59e95b4d
John Snow <thelunaticscripter@outlook.com>
2020-07-17 15:54:41 -0400

    More changes and work
    Signed-off-by: John Snow <thelunaticscripter@outlook.com>

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,6 +1,11 @@
 const Query = {
   resources(parent, args, { prisma }, info) {
     return prisma.query.resources(null, info)
+  },
+  agentExecutionList(parent, args, { prisma }, info) {
+    const resource = await prisma.query.resources(null, info)
+
+    const commandlist
   }
 }
 
